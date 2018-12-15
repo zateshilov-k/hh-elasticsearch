acf13e5117f26fc768231fc1976b0628d5fa6b71
Konstantin <headon1243@gmail.com>
2018-12-15 19:31:56 +0300

    Add lucene queries for visualisation, information about hh-api

--- test_cycle.sh
+++ test_cycle.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 export ES_URL=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' angry_noether):9200
-b=80000
+#b=80000
+num=231100
 i=0
-for d in ~/ParseHHnew/location_data/*; do
-	curl -X POST $ES_URL/my_index/vacancies/$i?pretty -d @$d
+for d in ~/ParseHHnew/location_data/*; do	
+	if [ "$i" -gt "$num" ]
+	then	  
+	  #curl -X POST $ES_URL/my_index/vacancies/$i?pretty -d @$d	  
+	fi
 	i=$((i+1))
 
-	if [ "$i" -eq "$b" ]
-	then
-	  echo "break"
-	  break
-	fi
+
 done
 curl -X GET $ES_URL/_cat/indices?v
-echo "$i"
+#echo "$i"
 echo "done"
